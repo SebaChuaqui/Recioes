e3ca59c211116805eca1c5b83efa689ebea7c179
Sebastian Chuaqui <schuaquicano@gmail.com>
2020-11-03 17:53:36 -0300

    parte 5

---
+++ src/main/java/com/example/myapplication/recipes/MyViewModel/ViewModelRecipes.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication.recipes.MyViewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.myapplication.recipes.repository.RepositoryRecipes
+import com.example.myapplication.recipes.room.Recetario
+import com.example.myapplication.recipes.room.RecipesDB
+import com.example.myapplication.recipes.room.RecipesDao
+
+
+class ViewModelRecipes(application: Application): AndroidViewModel(application) {
+
+    private val mRepositoryRecipes: RepositoryRecipes
+    val mAllRecipes: LiveData<List<Recetario>>
+
+    init {
+        val mRecipesDao = RecipesDB.getProductsDataBase(application).getRecipesDao()
+        mRepositoryRecipes = RepositoryRecipes(mRecipesDao)
+        mAllRecipes = mRepositoryRecipes.mRecipes
+        mRepositoryRecipes.getRecipesFromServer()
+
+    }
+
+    fun getOneID(id: String): LiveData<Recetario> {
+        return mRepositoryRecipes.getOneById(id)
+    }
+
+}
