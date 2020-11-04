03b32cc650481b429fba820ede552c15f472c8de
Sebastian Chuaqui <schuaquicano@gmail.com>
2020-11-03 22:58:47 -0300

    parte 11b

--- src/main/java/com/example/myapplication/recipes/retrofit/Recipes2.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.recipes.retrofit
-import com.example.myapplication.recipes.room.Recetario
-import com.google.gson.annotations.SerializedName
-
-data class Recipes2(
-    @SerializedName("recetario")
-    val recetario: List<Recetario>
-)
